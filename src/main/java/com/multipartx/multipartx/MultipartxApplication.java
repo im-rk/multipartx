d1a4b286b265c4f5899ccc816fe124570f0c55bf
im-rk <krramkumar2006g@gmail.com>
2026-06-15 20:01:00 +0530

    initial commit

---
+++ src/main/java/com/multipartx/multipartx/MultipartxApplication.java
@@ -0,0 +1,13 @@
+package com.multipartx.multipartx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipartxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipartxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/multipartx/multipartx/MultipartxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multipartx.multipartx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipartxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
